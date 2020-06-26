b060c7979394430a26f6e5223821c632fd215795
rainandray-netizen <ryan.d.adrian.27@gmail.com>
2020-06-26 18:24:57 -0400

    feat: added styling and pigeon access

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 
+class Pigeon:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, description, age):
+    self.name = name
+    self.description = description
+    self.age = age
+
+pigeons = [
+  Pigeon('Roy Pigeon', 'A hardworking salarypigeon trying to make ends meet. Husband to Sandra Pigeon', 3),
+  Pigeon('Sandra Pigeon', 'A proud mother, wife of Roy Pigeon', 4),
+  Pigeon('Egg', 'An egg', 0)
+]
 
 def about(request):
   return render(request, 'about.html')
 
 def home(request):
   return render(request, 'home.html')
+
+def pigeons_index(request):
+  return render(request, 'pigeons/index.html', { 'pigeons': pigeons })
